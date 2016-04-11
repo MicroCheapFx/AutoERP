7421d8b88b74bfe0775b4a79489411581ae83361
MicroCheapFx <fx@microcheap.info>
2016-04-11 11:57:49 +0200

    Ajout d'un système d'authentification

--- autoerp/main/urls.py
+++ autoerp/main/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
             # ex: /polls/
             url(r'^$', views.index, name='index'),
+            url(r'^login/$', views.user_login, name='login'),
+            url(r'^logout/$', views.user_logout, name='logout'),
+            #url(r'^accounts/login/', views.index, name='login'),
             # ex: /polls/5/
             #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
             # ex: /polls/5/results/
--- autoerp/main/views.py
+++ autoerp/main/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
+from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, logout, user_logged_in, login
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 
-#@login_required
+def user_login(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                #user_logged_in()
+                #return render(request, 'main/index.html', {})
+                return HttpResponseRedirect('/')
+            else:
+                #return render(request, 'autoerp/login.html', {})
+                return HttpResponse("Votre compte est désactivé.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            #return render(request, 'autoerp/login.html', {})
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'autoerp/login.html', {})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return render(request, 'autoerp/login.html', {})
+
+
+
+@login_required
 def index(request):
     return render(request, 'main/index.html', {})
 
